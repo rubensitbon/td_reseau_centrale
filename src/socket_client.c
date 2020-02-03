776ac52d37988cd28a617c60178951425c9a159a
Bro-nes <marquisvalentin@hotmail.fr>
2020-02-03 16:15:22 +0100

    Putain de commit de merde

--- src/socket_client.c
+++ src/socket_client.c
@@ -5,12 +5,25 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
 
 #define LOCAL_PORT 0
 #define MAX_CHAR 80
+#define NBMAX_THREADS 2
 
 #define CHECK_ERROR(var, val, message){if(var==val){perror( message);return -1;}}
+#define CHECK_T(status, msg)                        \
+  if (0 != (status))   {                            \
+    fprintf(stderr, "pthread erreur : %s\n", msg);  \
+    exit (-1);                                 \
+  }
 
+typedef void * (*pf_t)(void *);
+int sd; // Socket de dialogue
+char bufflect [MAX_CHAR+1];
+
+void threadSend(char * trame);
+void threadRead(void);
 
 int main(int argc, char **argv)
 {
@@ -25,39 +38,18 @@ int main(int argc, char **argv)
     char * IP_SERVEUR = argv[2];
     int PORT_SERVEUR = atoi(argv[3]);
     char * XWAY_ADDRESS = "1E";
+    pthread_t tid[NBMAX_THREADS];
 
     printf("IP_CLIENT: %s \n", IP_CLIENT);
     printf("IP_SERVEUR: %s \n", IP_SERVEUR);
     printf("PORT_SERVEUR: %d \n", PORT_SERVEUR);
     printf("XWAY_ADDRESS: %s \n", XWAY_ADDRESS);
 
-
-
-    int sd; // Socket de dialogue
     struct sockaddr_in adrlect;
     struct sockaddr_in adrecriv;
-    char bufflect [MAX_CHAR+1];
-    char buffecrit [MAX_CHAR+1];
     int tailleadr;
 
     char trame [27];
-
-    // Premeière trame
-//    trame[0] = 0x00;
-//    trame[1] = 0x00;
-//    trame[2] = 0x00;
-//    trame[3] = 0x01;
-//    trame[4] = 0x00;
-//    trame[5] = 0x08;
-//    trame[6] = 0x00;
-//    trame[7] = 0xF0;
-//    trame[8] = 0x1E;
-//    trame[9] = 0x10;
-//    trame[10] = 0x14;
-//    trame[11] = 0x10;
-//    trame[12] = 0x24;
-//    trame[13] = 0x06;
-
 
     // deuxième trame
 
@@ -112,21 +104,26 @@ int main(int argc, char **argv)
     adrlect.sin_port=htons(PORT_SERVEUR); // def du numero de port de la socket
     adrlect.sin_addr.s_addr=inet_addr(IP_SERVEUR); // def de l'addresse  de la socket
 
-    CHECK_ERROR(send(sd, (void *) trame, 26, 0), -1, "Erreur envoie data !!! \n");
+    CHECK_T(pthread_create (&tid[0], NULL, (pf_t)threadSend,
+                                (char *)trame), "pthread_create()");
+    CHECK_T(pthread_create (&tid[1], NULL, (pf_t)threadRead,
+                                NULL), "pthread_create()");
 
+    close(sd);
+    return 1;
+}
 
-    // printf("Bien joué roger !");
-     getchar();
+void threadSend(char * trame)
+{
+    CHECK_ERROR(send(sd, (void *) trame, 26, 0), -1, "Erreur envoi data !!! \n");
+    pthread_exit((void *) 1);
+}
 
+void threadRead(void)
+{
     while(1)
     {
         CHECK_ERROR(recv(sd, (void *) bufflect, MAX_CHAR+1, 0), -1, "Erreur reception data !!! \n");
         printf("Just Received> %s \n", bufflect);
-
     }
-
-    close(sd);
-
-
-    return 1;
 }
