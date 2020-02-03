a68e9a7352f79bb993156eba80cd67a9a28e0b69
Alexandre <alexandrethery1202@gmail.com>
2020-02-03 15:12:12 +0100

    Thread test

--- src/socket_client.c
+++ src/socket_client.c
@@ -5,12 +5,23 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
 
 #define LOCAL_PORT 0
 #define MAX_CHAR 80
+#define NBMAX_THREADS 2
 
 #define CHECK_ERROR(var, val, message){if(var==val){perror( message);return -1;}}
+#define CHECK_T(status, msg)                        \
+  if (0 != (status))   {                            \
+    fprintf(stderr, "pthread erreur : %s\n", msg);  \
+    exit (EXIT_FAILURE);                            \
+  }
 
+typedef void * (*pf_t)(void *);
+
+void threadSend(char * trame);
+void threadRead(void);
 
 int main(int argc, char **argv)
 {
@@ -25,6 +36,7 @@ int main(int argc, char **argv)
     char * IP_SERVEUR = argv[2];
     int PORT_SERVEUR = atoi(argv[3]);
     char * XWAY_ADDRESS = "1E";
+    pthread_t tid[NBMAX_THREADS];
 
     printf("IP_CLIENT: %s \n", IP_CLIENT);
     printf("IP_SERVEUR: %s \n", IP_SERVEUR);
@@ -95,21 +107,26 @@ int main(int argc, char **argv)
     adrlect.sin_port=htons(PORT_SERVEUR); // def du numero de port de la socket
     adrlect.sin_addr.s_addr=inet_addr(IP_SERVEUR); // def de l'addresse  de la socket
 
-    CHECK_ERROR(send(sd, (void *) trame, 26, 0), -1, "Erreur envoie data !!! \n");
+    CHECK_T(pthread_create (&tid[i], NULL, (pf_t)threadSend,
+                                (void *)i), "pthread_create()");
+    CHECK_T(pthread_create (&tid[i], NULL, (pf_t)threadRead,
+                                (void *)i), "pthread_create()");
 
+    close(sd);
+    return 1;
+}
 
-    // printf("Bien joué roger !");
-     getchar();
+void threadSend(char * trame)
+{
+    CHECK_ERROR(send(sd, (void *) trame, 26, 0), -1, "Erreur envoi data !!! \n");
+	pthread_exit((void *) 1);
+}
 
+void threadRead(void)
+{
     while(1)
     {
         CHECK_ERROR(recv(sd, (void *) bufflect, MAX_CHAR+1, 0), -1, "Erreur reception data !!! \n");
         printf("Just Received> %s \n", bufflect);
-
     }
-
-    close(sd);
-
-
-    return 1;
 }
